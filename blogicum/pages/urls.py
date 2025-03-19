7c19d68a4d167a4c1ce3cfc59766ca23bf40a0ee
ps <prodavecyada@MacBook-Pro-ps.local>
2025-03-19 21:16:42 +0300

    13

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('rules/', views.rules, name='rules'),
+    path('', views.about, name='about'),
+    path('', views.rules, name='rules'),
 ]
